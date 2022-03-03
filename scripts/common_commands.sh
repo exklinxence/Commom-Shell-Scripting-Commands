14ba9a5e083843bd156841b661d424105927b0dc
exklinxence <exklinxence@gmail.com>
2022-03-03 06:29:51 -0500

    Script to check if an application is installed
    Script checks if an app is install already or not

--- scripts/common_commands.sh
+++ scripts/common_commands.sh
@@ -1,4 +1,6 @@
-#How to check os version in Linux command line
+###How to check os version in Linux command lineoli@bert:/$ apt-cache policy firefox
+firefox:
+  Installed: 8.0+build1-0ubu
 
 cat /etc/os-release
 # Sample Output
@@ -36,3 +38,28 @@ hostnamectl
 #             Kernel: Linux 5.10.96-90.460.amzn2.x86_64
 #       Architecture: x86-64
 # [ec2-user@ip-10-0-1-101 ~]$ 
+
+### Check if a software is already installed
+apt-cache policy firefox
+## Sample output for installed application
+# firefox:
+#   Installed: (none)
+#   Candidate: 75.0+build3-0ubuntu1
+#   Version table:
+#      79.0+build1-0ubuntu0.20.04.1 -1
+#         100 /var/lib/dpkg/status
+#      75.0+build3-0ubuntu1 500
+#         500 http://archive.ubuntu.com/ubuntu focal/main amd64 Packages
+
+##Sample output not installed
+# jenkins:
+#   Installed: (none)
+#   Candidate: (none)
+#   Version table:
+
+##dpkg: dpkg -l | grep -E '^ii' | grep <package name>. When it's not installed it won't show output. When it is, it'll show something like:
+
+
+
+
+
